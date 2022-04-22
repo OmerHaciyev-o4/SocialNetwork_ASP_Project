f8bf3fb2a5dcdb0083f9455de091f3ab927f338e
OmerHaviyev-o4 <omer04118102@gmaiil.com>
2022-04-22 21:27:04 +0400

    Add project files.

---
+++ SocialNetwork.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetwork.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Badges()
+        {
+            return View();
+        }
+        public IActionResult Stories()
+        {
+            return View();
+        }
+        public IActionResult Videos()
+        {
+            return View();
+        }
+        public IActionResult Group()
+        {
+            return View();
+        }
+        public IActionResult Chat()
+        {
+            return View();
+        }
+        public IActionResult Settings()
+        {
+            return View();
+        }
+        public IActionResult AccountInformation()
+        {
+            return View();
+        }
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+        public IActionResult Notification()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Forgot()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult SearchResult()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SearchResult(string data)
+        {
+            return RedirectToAction("SearchResult");
+        }
+        public IActionResult GroupView()
+        {
+            return View();
+        }
+        public IActionResult LiveConversation()
+        {
+            return View();
+        }
+        public IActionResult Help()
+        {
+            return View();
+        }
+    }
+}
---
+++ SocialNetwork.WebUI/wwwroot/js/site.js
@@ -0,0 +1,63 @@
+﻿var openedNot = false;
+
+
+class STVariable {
+    static isDark = false;
+}
+
+document.body.reload = function() {
+    if (STVariable.isDark) {
+        console.log(STVariable.isDark);
+
+        document.getElementById("appModeIcon").classList.add("clicked");
+        document.body.classList.add("theme-dark");
+        STVariable.isDark = true;
+    }
+    else {
+        console.log(STVariable.isDark);
+
+        document.getElementById("appModeIcon").classList.remove("clicked");
+        document.body.classList.remove("theme-dark");
+        STVariable.isDark = false;
+    }
+}
+
+document.getElementById("dropdownMenu3").onclick = function () {
+    if (!openedNot) {
+        document.getElementById("dropdownMenu3").classList.add("show");
+        document.getElementById("notificationID").classList.add("show");
+        openedNot = true;
+    }
+    else {
+        document.getElementById("dropdownMenu3").classList.remove("show");
+        document.getElementById("notificationID").classList.remove("show");
+        openedNot = false;
+    }
+}
+
+document.getElementById("appMode").onclick = function () {
+    if (!STVariable.isDark) {
+        console.log(STVariable.isDark + "   1");
+
+        document.getElementById("appModeIcon").classList.add("clicked");
+        document.body.classList.add("theme-dark");
+        STVariable.isDark = true;
+    }
+    else {
+        console.log(STVariable.isDark + "   2");
+
+        document.getElementById("appModeIcon").classList.remove("clicked");
+        document.body.classList.remove("theme-dark");
+        STVariable.isDark = false;
+    }
+}
+
+document.getElementById('searchPanel').addEventListener("keydown", function (e) {
+    if (e.keyCode === 13) {
+        $.ajax({
+            url: '/home/searchresult?data=' + document.getElementById('searchPanel').value,
+            type: 'POST'
+        });
+        document.getElementById('searchPanel').value = '';
+    }
+});
